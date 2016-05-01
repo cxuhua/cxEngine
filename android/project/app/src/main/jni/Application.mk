6d6a4414ad6575f930d2ea9a2fa965fc636a2512
xuhua <cxuhua@gmail.com>
2016-05-01 19:36:55 +0800

    openal liuv update

--- android/project/app/src/main/jni/Application.mk
+++ android/project/app/src/main/jni/Application.mk
@@ -5,11 +5,8 @@ NDK_PATH := /Users/xuhua/android/android-ndk
 CX_ENGINE_PATH := /Users/xuhua/cxengine
 
 NDK_TOOLCHAIN_VERSION := clang3.6
-
 APP_STL := gnustl_static
-
-APP_PLATFORM := android-9
-
+APP_PLATFORM := android-14
 APP_ABI := armeabi
 
 NDK_MODULE_PATH += $(CX_ENGINE_PATH)

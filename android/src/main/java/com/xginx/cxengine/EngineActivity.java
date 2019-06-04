67f88f7b844d231ab965fbb1ccc9d8e440c3d7b2
xuhua <cxuhua@gmail.com>
2019-06-04 15:40:29 +0800

    save

--- android/src/main/java/com/xginx/cxengine/EngineActivity.java
+++ android/src/main/java/com/xginx/cxengine/EngineActivity.java
@@ -71,14 +71,9 @@ public class EngineActivity extends NativeActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) {
-                    View v = getWindow().getDecorView();
-                    v.setSystemUiVisibility(View.VISIBLE);
-                } else if (Build.VERSION.SDK_INT >= 19) {
-                    View decorView = getWindow().getDecorView();
-                    int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-                    decorView.setSystemUiVisibility(uiOptions);
-                }
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
             }
         });
 	}
@@ -88,14 +83,9 @@ public class EngineActivity extends NativeActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) {
-                    View v = getWindow().getDecorView();
-                    v.setSystemUiVisibility(View.GONE);
-                } else if (Build.VERSION.SDK_INT >= 19) {
-                    View decorView = getWindow().getDecorView();
-                    int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
-                    decorView.setSystemUiVisibility(uiOptions);
-                }
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
             }
         });
     }
